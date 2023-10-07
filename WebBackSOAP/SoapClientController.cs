b858a443b2a69b5fd40537545b924877573b6332
Estefany Rueda Romero <yurley.rueda.2019@upb.edu.co>
2023-10-07 16:28:18 -0500

    Consumiendo el los metodos del servidor JAVA (Localmente)

---
+++ WebBackSOAP/SoapClientController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceReference1;
+
+namespace WebBackSOAP
+{
+    public class SoapClientController : ControllerBase
+    {
+        [HttpGet]
+        [Route("CallSoapMethod")]
+        public IActionResult CallSoapMethod()
+        {
+            try
+            {
+                // Crea una instancia del cliente del servicio SOAP generado.
+                using (var client = new ServiceClient())
+                {
+                    // Llama a un método del servicio SOAP.
+                    var result = client.loginAsync;
+
+                    // Procesa la respuesta y devuelve los datos necesarios.
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Maneja las excepciones adecuadamente.
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
